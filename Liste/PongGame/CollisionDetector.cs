5f83a921660fe792a593f3f4c5e34eb228b6e50d
Hrvoje <hrvojekov555@gmail.com>
2015-12-14 00:00:28 +0100

    Zadnji commit... malo popravljeno :)

--- Liste/PongGame/CollisionDetector.cs
+++ Liste/PongGame/CollisionDetector.cs
@@ -17,9 +17,9 @@ namespace PongGame
         ///<returns>Returns true if overlapping</returns>
         public static bool Overlaps(Sprite s1, Sprite s2)
         {
-            if ((s1.Position.Y + (Ball.BallSize / 2.0f) >= 900.0f - 30.0f && s1.Position.X >= s2.Position.X && 
+            if ((s1.Position.Y + Ball.BallSize >= 900.0f - s2.Size.Height && s1.Position.X >= s2.Position.X && 
                 s1.Position.X + Ball.BallSize <= s2.Position.X + s2.Size.Width) ||
-                (s1.Position.Y  <= s2.Size.Height && s1.Position.X >= s2.Position.X &&
+                (s1.Position.Y  <= s2.Size.Height && s1.Position.X >= s2.Position.X && 
                 s1.Position.X + Ball.BallSize <= s2.Position.X + s2.Size.Width))
                 return true;
 
